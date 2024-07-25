29847aafd0fe5ef8388449ec5320620e2696285f
Andrew McConachie <smutt@depht.com>
2024-07-25 13:32:21 +0000

    working. likely buggy.

--- idk.php
+++ idk.php
@@ -3,8 +3,10 @@
 // Replacement for doodle.com in a single PHP file
 
 $MAX_RESPONSES = 20; // Maximum number of responses we'll accept
+$MIN_CHARS_NAME = 3; // Minimum number of characters allowed in name
 $MAX_CHARS_NAME = 10; // Maximum number of characters allowed in name
 $BAD_HOMBRES = array("conf"); // Disallowed nombres
+$ANIMALS_FILE = "animals_sha256.txt"; // Animals and their SHA256 hash sorted by hash ascending
 
 error_reporting(E_ALL);
 
@@ -69,23 +71,25 @@ function check_nombre($txt){
   if( in_array($txt, $GLOBALS['BAD_HOMBRES'])){
     return false;
   }
+  if(strlen($txt) < $GLOBALS['MIN_CHARS_NAME']){
+    return false;
+  }
+  if(strlen($txt) > $GLOBALS['MAX_CHARS_NAME']){
+    return false;
+  }
   return true;
 }
 
 // Takes the contents of the conf file
 // Returns assoc array
 function parse_conf($txt){
-  if( $txt == false){
-    bug_out("Bad conf parse");
-  }
-
   $rv = array();
   foreach(explode(PHP_EOL, $txt) as $line){
     $line = trim($line);
-    if( strlen($line) == 0){
+    if(strlen($line) == 0){
       continue;
     }
-    if( strpos($line, ":") === 0){
+    if(strpos($line, ":") === 0){
       $idx = trim($line, ": ");
       $rv[$idx] = array();
       continue;
@@ -95,10 +99,30 @@ function parse_conf($txt){
   return $rv;
 }
 
+// Takes a string
+// Return animal string associated with next ascending hash
+function match_animal($txt){
+  $animals = array(hash("sha256", $txt, false) => "");
+  foreach(file($GLOBALS['ANIMALS_FILE']) as $line){
+    list($hash, $critter) = explode(" ", $line);
+    $animals[$hash] = $critter;
+  }
+
+  ksort($animals);
+  reset($animals);
+  while(next($animals) !== false){
+    if(current($animals) == ""){
+      if(next($animals) != null){
+        return current($animals);
+      }else{
+        return array_shift($animals);
+      }
+    }
+  }
+}
+
 // Deal with POST data
 function process_post($data){
-  var_dump($data);
-
   if( !isset($_POST['ding'])){
     bug_out("No ding in POST");
   }
@@ -120,7 +144,7 @@ function process_post($data){
     $nombre = $_POST['nombre'];
   }
 
-  $output = "";
+  $fileput = "Name :: " . $nombre . "\n\n";
   foreach($_POST as $key => $val){
     if($key == 'ding' || $key == 'nombre' || $key == 'sendit'){
       continue;
@@ -129,36 +153,40 @@ function process_post($data){
     if( !in_array($choice, $conf['choices'], true)){
       bug_out("Bogus choice given in POST");
     }else{
-      $output .= $choice . " " . $val . "\n";
+      $fileput .= $choice . " :: " . $val . "\n";
     }
   }
-  print($output);
-  $files = scandir($ding);
-  var_dump($files);
+  $fileput .= "Animal :: " . match_animal($fileput) . "\n";
 
+  // Write fileput to file
+  // Overwrite existing file if same nombre
+  $files = scandir($ding);
   if(in_array($nombre, $files)){
-    file_put_contents($ding . "/" . $nombre, $output, LOCK_EX);
+    file_put_contents($ding . "/" . $nombre, $fileput, LOCK_EX);
   }else{
-    if(count($files) + 3 < $GLOBALS['MAX_RESPONSES']){
-      file_put_contents($ding . "/" . $nombre, $output, LOCK_EX);
+    if(count($files) - 2 - count($GLOBALS['BAD_HOMBRES']) < $GLOBALS['MAX_RESPONSES']){
+      file_put_contents($ding . "/" . $nombre, $fileput, LOCK_EX);
     }else{
       print("Max responses received");
       bug_out("Max responses received");
     }
   }
 
+  // Print webput to user
+  $webput = "<html><head></head><body>";
+  foreach(explode("\n", $fileput) as $line){
+    $webput .= "\n" . $line . "<br/>";
+  }
+  $webput .= "\n</body><html>";
+  print($webput);
 }
 
-// Return animal string from file
-function animal($txt){
-  return "";
-}
-
-// BEGIN EXECUTION
+/////////////////////
+// BEGIN EXECUTION //
+/////////////////////
 if(isset($_POST['nombre'])){
   if(strlen($_POST['nombre']) > 0){
     process_post($_POST);
-    print "\nThanks for your input!";
     exit(0);
   }else{
     $_GET['ding'] = $_POST['ding']; // disgusting hack :)
@@ -174,14 +202,16 @@ if(! check_ding($_GET['ding'])){
 }
 $ding = $_GET['ding'];
 
-$conf = parse_conf(trim(file_get_contents($ding . "/conf")));
+if( !($cf = file_get_contents($ding . "/conf"))){
+  bug_out("Unreadable conf file " . $ding);
+}
+$conf = parse_conf(trim($cf));
 
 $closing = explode("-", $conf['closing'][0]);
 if(time() > mktime(0, 0, 0, $closing[1], $closing[2], $closing[0])){
   print("Responses are closed");
   bug_out("Late response attempt");
 }
-
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
@@ -198,7 +228,7 @@ print $conf['title'][0];
 print " (Repeat same name to overwrite previous)";
 print "\n<table>";
 print "<tr><td>Name </td><td> <input type=\"text\" id=\"nombre\" name=\"nombre\"></td>";
-print "<td>(ASCII letters and numbers only, max " . $MAX_CHARS_NAME . ")</td></tr>";
+print "<td>(ASCII letters and numbers only, min " . $MIN_CHARS_NAME . " max " . $MAX_CHARS_NAME . ")</td></tr>";
 
 print "\n<tr><td></td>";
 foreach($conf['options'] as $option){
